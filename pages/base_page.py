7990371c9fb563f7ede9c81816519f949f89376e
Majtreja <d.bielov@gmail.com>
2019-07-16 16:19:43 +0300

    assertion added

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.common.exceptions import NoSuchElementException
 
 
 class BasePage(object):
@@ -6,6 +7,14 @@ class BasePage(object):
     def open(self):
         self.browser.get(self.url)
 
-    def __init__(self, browser, url):
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
+
+    def __init__(self, browser, url, timeout=10):
         self.browser: WebDriver = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
