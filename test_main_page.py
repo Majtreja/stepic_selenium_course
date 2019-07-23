164919699bd82b8e09121f0783a86d5f90c3ab03
Majtreja <d.bielov@gmail.com>
2019-07-23 11:17:41 +0300

    login guest mark added

--- test_main_page.py
+++ test_main_page.py
@@ -1,25 +1,39 @@
+import pytest
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 from .pages.base_page import BasePage
 from .pages.cart_page import CartPage
 
 
+
 # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
 link = "http://selenium1py.pythonanywhere.com"
 
 
-def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
+@pytest.mark.login_guest
+class TestLoginFromMainPage(object):
+    # @pytest.fixture(scope="function", autouse=True)
+    # def setup(self):
+    #     self.product = ProductFactory(title = "Best book created by robot")
+    #     # создаем по апи
+    #     self.link = self.product.link
+    #     yield
+    #     # после этого ключевого слова начинается teardown
+    #     # выполнится после каждого теста в классе
+    #     # удаляем те данные, которые мы создали
+    #     self.product.delete()
 
+    def test_guest_can_go_to_login_page(self, browser):
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
-def test_guest_should_see_login_link(browser):
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_should_see_login_link(self, browser):
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
 
 def test_login_page(browser):
